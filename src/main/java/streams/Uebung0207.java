c3e608f9d5376e5c9785ac25370aed573dc59f4d
anne.quinkenstein@tib.eu <anne.quinkenstein@tib.eu>
2024-07-02 20:20:35 +0200

    added powersOfTwoList()

--- src/main/java/streams/Uebung0207.java
+++ src/main/java/streams/Uebung0207.java
@@ -18,12 +18,27 @@ public class Uebung0207 {
     public static IntStream powersOfTwo() {
        return IntStream.range(4, 9).map(x -> (int)Math.pow(2.0, x))
                                         .filter(x -> x >= 20 && x <= 230);
-                                        //.collect(Collectors.toList());
+    }
+
+
+    public static List<Integer> powersOfTwoList() {
+        return IntStream.range(4, 9).map(x -> (int)Math.pow(2.0, x))
+                        .filter(x -> x >= 20 && x <= 230)
+                         .boxed()
+                        //.boxed() hinzugefügt, um aus einem IntStream einen Stream<Integer> zu machen.
+                        //Eine Collection (Liste) geht nicht mit primitiven Datentypen (int) nur Integer.
+                        //.map(x-> Integer.valueOf(x)) geht nicht->
+                        // map wird nur für int-zu-int-Mapping benutzt, deswegen hat das nicht funktioniert. Es gäbe mapToObj
+                        // .mapToObj(x -> Integer.valueOf(x))
+                        .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
-        powersOfTwo().forEach(x -> System.out.println(x));
 
+        System.out.println("Print des gesamten IntSteams: " + powersOfTwo());
+        powersOfTwo().forEach(x -> System.out.println("hier wird aus dem IntStream geprintet: " + x));
+
+        System.out.println("Print der gesamten Liste: " + powersOfTwoList());
     }
 
 }
